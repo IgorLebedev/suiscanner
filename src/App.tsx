a862823baa5dca0ae0caab8d6e0c938b1d00ee28
igorlebedev <igorlebedev228@gmail.com>
2023-07-05 17:18:00 +0700

    add showcase page & store provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import './App.css';
-import Form from './components/Form';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { store } from './slicers/store';
+import { Provider } from 'react-redux';
+import Form from './components/Form';
+import ShowCase from './components/ShowCase';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Form/>} /> 
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Form/>} /> 
+          <Route path="/showcase" element={<ShowCase/>} /> 
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
