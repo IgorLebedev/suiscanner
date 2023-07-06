4eff2d09e39e9c776e703e00e80450bd597392e1
igorlebedev <igorlebedev228@gmail.com>
2023-07-06 13:25:07 +0700

    minor fix

--- src/components/NftCard.tsx
+++ src/components/NftCard.tsx
@@ -1,8 +1,8 @@
   const Card = ({ link }: { link: string }) => {
   return (
-    <div className="w-auto p-3 col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 rounded-lg bg-slate-300 transition-all hover:-translate-x-2 hover:-translate-y-2">
+    <div className="p-3 col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 rounded-lg bg-slate-300 transition-all hover:-translate-x-2 hover:-translate-y-2">
       <div className="flex justify-center items-center m-auto">
-        <img src={link} alt='...' />
+        <img src={link} alt='...' className="rounded-md" />
       </div>
     </div>
   );
